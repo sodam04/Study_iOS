973619f4297d257609958ca9795340d5a0667e13
sodam04 <sdsd.10203@gmail.com>
2022-07-23 22:59:29 +0900

    [CREATE]BASIC_IOS

---
+++ Basic_iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Basic_iOS
+//
+//  Created by 김소담 on 2022/07/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
