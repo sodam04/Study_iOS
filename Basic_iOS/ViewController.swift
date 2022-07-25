9e7cebc80905bae7f549ef8945e6bc242aa4ef6f
sodam04 <sdsd.10203@gmail.com>
2022-07-25 23:59:33 +0900

    [ADD]Image_Veiw

--- Basic_iOS/ViewController.swift
+++ Basic_iOS/ViewController.swift
@@ -8,10 +8,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var imgView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        imgView.image = UIImage(named: "hr.png" )
     }
 
 
